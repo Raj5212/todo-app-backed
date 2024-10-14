906ef31c4b542925780577779f4bee194114394e
Raj <raj@rajs-MacBook-Air.local>
2024-10-14 17:26:16 +0530

    initial commit

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -5,8 +5,8 @@ const middleware = require("../middleware/auth");
 
 router.post("/add-task", middleware.authUser, taskController.createTask );
 router.get("/view-task", middleware.authUser, taskController.listTask);
-router.put ("/update-task" ,middleware.authUser ,  taskController.updateTask)
-router.delete ("/delete-task" ,middleware.authUser ,  taskController.deleteTask)
+router.put ("/update-task/:id" ,middleware.authUser ,  taskController.updateTask)
+router.delete ("/delete-task/:id" ,middleware.authUser ,  taskController.deleteTask)
 
 
 module.exports = router;
